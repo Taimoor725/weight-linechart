d91933c950d9484c9edb74d1d182072bbaa842ad
ch-moor <taimoor725tanveer@gmail.com>
2025-04-14 10:56:24 -0700

    ui adjustments

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import WeightChart from "@/components/WeightChart";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen justify-center items-center bg-black">
+    <div className="flex h-screen w-screen lg:p-0 p-2 justify-center items-center bg-black">
       
         <WeightChart/>
     </div>
--- components/WeightChart.js
+++ components/WeightChart.js
@@ -6,7 +6,7 @@ import {
 } from 'recharts';
 
 const InitialWeight = 80;
-const TargetLoss =5
+const TargetLoss = 5;
 const FinalWeight = InitialWeight - 5;
 const TotalWeeks = 10;
 
@@ -21,47 +21,11 @@ const generateEvenWeekData = () => {
 
 const data = generateEvenWeekData();
 
-
-const CustomTooltip = ({ active, payload }) => {
-    if (active && payload && payload.length) {
-        const value = payload[0].value;
-        return (
-            <div style={{
-                width: '8rem',
-                height: '8rem',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                color: 'black',
-                fontWeight: 'bold',
-                fontSize: '0.875rem',
-                textAlign: 'center',
-            }}>
-                <div style={{
-                    backgroundColor: '#a3e635',
-                    width: '6rem',
-                    height: '6rem',
-                    borderRadius: '9999px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                    Ziel<br />{value} kg
-                </div>
-            </div>
-        );
-    }
-
-    return null;
-};
-
 export default function WeightChart() {
     const targetWeight = InitialWeight - TargetLoss;
 
     return (
-        <div className="bg-[#f5f8f8] p-4 rounded-[40px] w-2/3 h-2/3 flex justify-center items-center mx-auto relative shadow-sm">
+        <div className="bg-[#f5f8f8] lg:p-4 lg:rounded-[40px] rounded-2xl w-full lg:w-2/3 h-1/2 lg:h-2/3 flex justify-center items-center lg:mx-auto relative shadow-sm overflow-hidden">
             <ResponsiveContainer width="100%" height="90%">
                 <AreaChart data={data} margin={{ top: 30, right: 20, left: 0, bottom: 0 }}>
                     <defs>
@@ -70,32 +34,31 @@ export default function WeightChart() {
                             <stop offset="100%" stopColor="#4ea5f7" stopOpacity={0} />
                         </linearGradient>
                     </defs>
-                    <CartesianGrid strokeDasharray="4" vertical={false} />
+                    <CartesianGrid strokeDasharray="4 0" vertical={false} />
                     <XAxis
                         dataKey="week"
-                        domain={[0, 10]} // ensures full 0–10 range
+                        domain={[0, 10]}
                         ticks={[0, 2, 4, 6, 8, 10]}
                         type="number"
-                        tick={{ fontSize: 16, fontWeight: 600 }}
+                        tick={{ fontSize: '1.4rem', fontWeight: 600 }}
                         tickLine={false}
                         axisLine={false}
                         label={{ value: '', position: 'insideRight', offset: -20 }}
                     />
-
                     <YAxis
-                        tick={{ fontSize: 16, fontWeight: 600 }}
+                        tick={{ fontSize: '1.3rem', fontWeight: 600 }}
                         domain={[targetWeight, InitialWeight]}
                         interval={0}
                         ticks={Array.from({ length: TargetLoss + 1 }, (_, i) => InitialWeight - i)}
                         label={{ value: 'kg', angle: -90, position: 'insideLeft', offset: 10 }}
                     />
 
-                    <Tooltip content={<CustomTooltip />} />
+                    <Tooltip />
                     <Area
                         type="monotone"
                         dataKey="weight"
                         stroke="#4ea5f7"
-                        strokeWidth={5}
+                        strokeWidth={3}
                         fill="url(#weightGradient)"
                         dot={false}
                         activeDot={false}
@@ -103,19 +66,17 @@ export default function WeightChart() {
                 </AreaChart>
             </ResponsiveContainer>
 
-            {/* kg bubble */}
-            <div className="absolute top-3 left-5 bg-blue-500 text-white px-3 py-1 rounded-full font-bold text-sm">
+            <div className="absolute top-1 left-6 lg:top-7 lg:left-10 bg-blue-500 text-white px-2 py-1 md:px-3 md:py-1 rounded-full font-bold text-xs md:text-sm">
                 kg
             </div>
 
-            {/* Woche bubble */}
-            <div className="absolute right-3 bottom-[3rem] bg-blue-500 text-white px-4 py-1 rounded-full font-bold text-sm">
+            <div className="absolute lg:bottom-[2.2rem] right-0.5 bottom-[1.2rem] bg-blue-500 text-white px-2 py-1 lg:px-4 lg:py-2 rounded-full font-bold text-[10px] lg:text-lg">
                 Woche
             </div>
 
-            <div className='absolute right-[11.5rem] bottom-[10rem] flex flex-col gap-0 justify-center items-center'>
+            <div className='absolute right-[1rem] bottom-[5rem] lg:right-[12.5rem] lg:bottom-[9.5rem] z-10 flex flex-col gap-0 justify-center items-center'>
                 <div style={{
-                    width: '8rem',
+                    width: '6rem',
                     height: 'auto',
                     display: 'flex',
                     flexDirection: 'column',
@@ -123,25 +84,22 @@ export default function WeightChart() {
                     justifyContent: 'center',
                     color: 'black',
                     fontWeight: 'bold',
-                    fontSize: '0.875rem',
+                    fontSize: '0.7rem',
                     textAlign: 'center',
                 }}>
-                    <div style={{
-                        backgroundColor:'#C9CBCB',
-                        width: '7rem',
-                        height: '7rem',
-                        color:'black',
+                    <div className='lg:w-[6rem] lg:h-[6rem] w-[4rem] h-[4rem]' style={{
+                        backgroundColor: '#9FFB04',
+                        color: 'black',
                         borderRadius: '9999px',
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                         justifyContent: 'center'
                     }}>
-                        {/* <div className=''>Final Goal</div> */}
-                        <span className='text-xl font-normal'>Ziel<br/> {targetWeight} kg</span>
+                        <span className='text-xs md:text-xl font-normal'>Ziel<br/><span className='text-sm md:text-2xl font-semibold'> {targetWeight} kg</span></span>
                     </div>
                 </div>
-                <div className='w-5 h-5 border-[4px] border-black  rounded-full'></div>
+                <div className='w-4 h-4 md:w-5 md:h-5 border-[1px] bg-white border-gray-400 shadow-xl rounded-full'></div>
             </div>
         </div>
     );
